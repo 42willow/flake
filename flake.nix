cfbeb59a6723b73cec100766434a3ee6e7c0bd3a
willow <42willow@pm.me>
2024-12-31 17:58:51 +1100

    docs: add todo

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "NixOS configuration of 42Willow";
   inputs = {
+    # TODO: reduce inputs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
