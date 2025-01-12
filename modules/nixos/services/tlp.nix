32c28536c8069cf616f02162cc2577e8adb40a16
willow <42willow@pm.me>
2025-01-13 10:34:26 +1100

    fix: attempt to prevent samba hanging

--- modules/nixos/services/tlp.nix
+++ modules/nixos/services/tlp.nix
@@ -6,6 +6,8 @@
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "low-power";
+      WIFI_PWR_ON_AC = "off"; # default: off
+      WIFI_PWR_ON_BAT = "off"; # default: on  todo)) trying to resolve samba frozen unit session slice issue
     };
   };
 }
