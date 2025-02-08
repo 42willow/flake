cfc68a7ffa265404175d716537f43b9edef9956b
willow <42willow@pm.me>
2025-02-09 10:16:01 +1100

    refactor: use other usb-c port

--- modules/home/desktop/hyprland/hyprpaper.nix
+++ modules/home/desktop/hyprland/hyprpaper.nix
@@ -13,6 +13,7 @@
       wallpaper = [
         "eDP-1, ${inputs.wallpapers.packages.${pkgs.system}.macchiato}/share/wallpapers/macchiato/images/art/kurzgesagt/black_hole_1.png"
         "DP-1, ${inputs.wallpapers.packages.${pkgs.system}.macchiato}/share/wallpapers/macchiato/images/art/kurzgesagt/black_hole_2.png"
+        "DP-2, ${inputs.wallpapers.packages.${pkgs.system}.macchiato}/share/wallpapers/macchiato/images/art/kurzgesagt/black_hole_2.png"
       ];
       splash = false;
     };
