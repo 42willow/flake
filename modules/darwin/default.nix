aa95cbf6df373e34b6c130623ad08b7653005a4c
willow <42willow@pm.me>
2025-08-21 17:10:21 +1000

    feat(darwin): update defaults

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -10,8 +10,25 @@ in {
 
     primaryUser = user.name;
 
-    # defaults = {
-    # };
+    defaults = {
+      finder = {
+        AppleShowAllExtensions = true; # show all file extensions
+        FXDefaultSearchScope = "SCcf"; # search current folder by default
+        FXEnableExtensionChangeWarning = false; # disable file extension changing warning
+        FXPreferredViewStyle = "Nlsv"; # list view
+        FXRemoveOldTrashItems = true; # empty bin after 30 days
+        QuitMenuItem = true; # allow cmd+q
+        ShowPathbar = true; # show breadcrumbs
+        ShowStatusBar = true; # bottom status bar
+      };
+      dock = {
+        # hot corners
+        wvous-bl-corner = 5; # screen saver
+        wvous-br-corner = 10; # put display to sleep 
+        wvous-tl-corner = 2; # mission control
+        wvous-tr-corner = 12; # notification centre
+      };
+    };
   };
 
   # touchID for sudo authentication
