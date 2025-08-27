1f6a04b95595396982f74d4605bca0e980497f9d
willow <42willow@pm.me>
2025-08-27 17:32:43 +1000

    refactor: simplify ghostty launch binding to open new instances

--- modules/darwin/desktop/skhd.nix
+++ modules/darwin/desktop/skhd.nix
@@ -15,7 +15,7 @@
       alt + shift - i : yabai -m display --focus prev
       alt + shift - o : yabai -m space --focus next
 
-      alt - q : id=$(yabai -m query --windows | jq -re ".[] | select(.app == \"Ghostty\") | .id" | head -n 1) && [ -z "$id" ] && open -a Ghostty || (yabai -m window --focus $id && skhd -k "cmd - n")
+      alt - q : open -a Ghostty -n
 
       ${builtins.concatStringsSep "\n" (
         builtins.genList (
