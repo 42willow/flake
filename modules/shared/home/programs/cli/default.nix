055393d27a6a724badbef7c3331844048dd403f2
willow <42willow@pm.me>
2025-11-28 16:45:48 +1100

    feat(programs): uv

--- modules/shared/home/programs/cli/default.nix
+++ modules/shared/home/programs/cli/default.nix
@@ -1,5 +1,25 @@
 {
+  lib,
+  inputs,
+  pkgs,
+  osConfig,
+  ...
+}: let
+  inherit (lib) optionals concatLists;
+  cfg = osConfig.settings.programs;
+in {
   imports = [
     ./git.nix
   ];
+
+  config = lib.mkIf cfg.cli.enable {
+    home.packages = with pkgs;
+    with inputs;
+      concatLists [
+        (optionals cfg.categories.dev.enable [
+          # python
+          uv
+        ])
+      ];
+  };
 }
