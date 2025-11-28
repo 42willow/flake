6622c9ba27db907101d20a3d116988e139ed86b2
willow <42willow@pm.me>
2025-11-28 18:50:28 +1100

    feat(gitui): keymap

--- modules/shared/home/programs/tui/gitui.nix
+++ modules/shared/home/programs/tui/gitui.nix
@@ -7,6 +7,20 @@
 in {
   config = lib.mkIf (cfg.tui.enable
     && cfg.categories.dev.enable) {
-    programs.gitui.enable = true;
+    programs.gitui = {
+      enable = true;
+
+      keyConfig = ''
+        (
+          move_left: Some(( code: Char('n'), modifiers: "" )),
+          move_down: Some(( code: Char('e'), modifiers: "" )),
+          move_up: Some(( code: Char('i'), modifiers: "" )),
+          move_right: Some(( code: Char('o'), modifiers: "" )),
+          status_ignore_file: Some(( code: Char('g'), modifiers: "" )),
+          edit_file: Some(( code: Char('m'), modifiers: "" )),
+          open_options: Some(( code: Esc, modifiers: "" )),
+        )
+      '';
+    };
   };
 }
