8162070e7300a9a9c3ec546a015726c143896cc0
willow <42willow@pm.me>
2025-12-24 18:35:56 +1100

    style: format options

--- modules/shared/options/default.nix
+++ modules/shared/options/default.nix
@@ -11,110 +11,67 @@ in {
     programs = {
       enable =
         mkEnableOption "Enable all programs"
-        // {
-          default = true;
-        };
+        // {default = true;};
 
-      cli = {
-        enable =
-          mkEnableOption "Enable CLI programs"
-          // {
-            default = cfg.programs.enable;
-          };
-      };
-      gui = {
-        enable =
-          mkEnableOption "Enable GUI programs"
-          // {
-            default = cfg.desktop.enable;
-          };
-      };
-      tui = {
-        enable =
-          mkEnableOption "Enable TUI programs"
-          // {
-            default = cfg.programs.enable;
-          };
-      };
+      cli.enable =
+        mkEnableOption "Enable CLI programs"
+        // {default = cfg.programs.enable;};
+      tui.enable =
+        mkEnableOption "Enable TUI programs"
+        // {default = cfg.programs.enable;};
+      gui.enable =
+        mkEnableOption "Enable GUI programs"
+        // {default = cfg.desktop.enable;};
 
       categories = {
         enable =
           mkEnableOption "Enable all program categories"
-          // {
-            default = cfg.programs.enable;
-          };
+          // {default = cfg.programs.enable;};
         core.enable =
           mkEnableOption "Enable core programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         design.enable =
           mkEnableOption "Enable design programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         dev.enable =
           mkEnableOption "Enable development programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         edu.enable =
           mkEnableOption "Enable educational programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         fs.enable =
           mkEnableOption "Enable file management programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         fun.enable =
           mkEnableOption "Enable fun programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         gaming.enable =
           mkEnableOption "Enable gaming programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         mail.enable =
           mkEnableOption "Enable email programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         media.enable =
           mkEnableOption "Enable multimedia programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         music.enable =
           mkEnableOption "Enable music programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         printing.enable =
           mkEnableOption "Enable 3D printing programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         privacy.enable =
           mkEnableOption "Enable privacy programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         tools.enable =
           mkEnableOption "Enable QOL programs"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
         web.enable =
           mkEnableOption "Enable browsers"
-          // {
-            default = cfg.programs.categories.enable;
-          };
+          // {default = cfg.programs.categories.enable;};
       };
     };
+
     system = {
       user = {
         name = mkOption {
@@ -133,70 +90,51 @@ in {
           default = "users";
         };
       };
+
       hostName = mkOption {
         type = types.str;
         description = "The hostname of your system";
         default = "nixos";
       };
+
       services = {
         enable =
           mkEnableOption "Enable system services"
-          // {
-            default = true;
-          };
+          // {default = true;};
         bluetooth.enable =
           mkEnableOption "Enable Bluetooth"
-          // {
-            default = cfg.system.services.enable;
-          };
+          // {default = cfg.system.services.enable;};
         sound.enable =
           mkEnableOption "Enable sound"
-          // {
-            default = cfg.system.services.enable;
-          };
+          // {default = cfg.system.services.enable;};
         sync.enable =
           mkEnableOption "Enable syncthing"
-          // {
-            default = false;
-          };
+          // {default = false;};
         printing.enable =
           mkEnableOption "Enable printing"
-          // {
-            default = cfg.system.services.enable;
-          };
+          // {default = cfg.system.services.enable;};
         networking.enable =
           mkEnableOption "Enable networking"
-          // {
-            default = cfg.system.services.enable;
-          };
+          // {default = cfg.system.services.enable;};
         security.enable =
           mkEnableOption "Enable security"
-          // {
-            default = cfg.system.services.enable;
-          };
+          // {default = cfg.system.services.enable;};
         backups.enable =
           mkEnableOption "Enable restic"
-          // {
-            default = false;
-          };
+          // {default = false;};
       };
     };
+
     desktop = {
       enable =
         mkEnableOption "Enable desktop environment"
-        // {
-          default = true;
-        };
+        // {default = true;};
       niri.enable =
         mkEnableOption "Enable Niri twm"
-        // {
-          default = cfg.desktop.enable;
-        };
+        // {default = cfg.desktop.enable;};
       hyprland.enable =
         mkEnableOption "Enable Hyprland and its ecosystem"
-        // {
-          default = cfg.desktop.enable;
-        };
+        // {default = cfg.desktop.enable;};
     };
   };
 }
