725d5091ee10649b6d18667dd5e7f0fb1cf0e6a9
willow <42willow@pm.me>
2026-06-20 16:20:32 +1000

    improve variable names

--- modules/lib/default.nix
+++ modules/lib/default.nix
@@ -35,18 +35,18 @@ in {
 
       # walk the tree recursively
       #
-      # prefix:
+      # treePath:
       # relative path from tree root
       # grows on every decent (groups + plain attrsets)
       # e.g. ["terminal" "tools"]
       #
-      # ancestors:
+      # parentEnablePaths:
       # paths of every parent group enable
       # grows on group nodes (mkProgramGroup)
       # e.g. [["nest" "programs" "terminal" "tools" "enable"]]
       #
-      # plain attrsets don't add ancestors, but prefix still advances
-      walk = prefix: ancestors:
+      # plain attrsets don't add parentEnablePaths, but treePath still advances
+      walk = treePath: parentEnablePaths:
         builtins.mapAttrs (
           name: value:
             if value ? programType
@@ -57,29 +57,29 @@ in {
               default =
                 config.nest.programs.enable # global master
                 && config.nest.programs.${typ} # type (e.g. nest.programs.cli)
-                && builtins.all (x: x) (map get ancestors); # all parent groups enabled
+                && builtins.all (x: x) (map get parentEnablePaths); # all parent groups enabled
               defaultText =
                 "nest.programs.enable and nest.programs.${typ}"
-                + optionalString (ancestors != [])
-                (" and " + concatStringsSep " and " (map (concatStringsSep ".") ancestors));
+                + optionalString (parentEnablePaths != [])
+                (" and " + concatStringsSep " and " (map (concatStringsSep ".") parentEnablePaths));
             in
-              trace "PROGRAM: name=${name}, prefix=[${concatStringsSep "," prefix}], ancestors=[${concatStringsSep "; " (map (concatStringsSep ".") ancestors)}], default=${builtins.toString default}"
+              trace "PROGRAM: name=${name}, treePath=[${concatStringsSep "," treePath}], parentEnablePaths=[${concatStringsSep "; " (map (concatStringsSep ".") parentEnablePaths)}], default=${builtins.toString default}"
               {enable = mkEnableOption desc // {inherit default defaultText;};}
               // builtins.removeAttrs value ["programType" "description" "__functor"]
             # group node
             else if value ? children
             then let
-              fullPath = prefix ++ [name]; # e.g. ["terminal" "tools" "ripgrep"]
+              fullPath = treePath ++ [name]; # e.g. ["terminal" "tools" "ripgrep"]
               ancestorPath = ["nest" "programs"] ++ fullPath ++ ["enable"];
             in
               trace "GROUP: name=${value.name}, fullPath=[${concatStringsSep "," fullPath}], ancestorPath=[${concatStringsSep "," ancestorPath}]"
               {enable = mkEnableOption value.name;}
-              // walk fullPath (ancestors ++ [ancestorPath]) (value.children)
+              // walk fullPath (parentEnablePaths ++ [ancestorPath]) (value.children)
             # plain attrset (structural grouping without option)
             else if builtins.isAttrs value
             then
-              trace "ATTRSET: name=${name}, prefix=[${concatStringsSep "," prefix}] recursing"
-              (walk (prefix ++ [name]) ancestors value)
+              trace "ATTRSET: name=${name}, treePath=[${concatStringsSep "," treePath}] recursing"
+              (walk (treePath ++ [name]) parentEnablePaths value)
             # pass through unchanged
             else trace "VALUE: name=${name} (non-attrs, passing through)" value
         );
