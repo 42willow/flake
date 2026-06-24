1baa84ba32c18304d454bae71228f4ec36146a11
willow <42willow@pm.me>
2026-06-24 21:38:36 +1000

    programs: chiri

--- modules/shared/home/programs/gui/default.nix
+++ modules/shared/home/programs/gui/default.nix
@@ -1,6 +1,20 @@
 {
+  lib,
+  osConfig,
+  pkgs,
+  inputs,
+  ...
+}: let
+  cfg = osConfig.settings.programs;
+in {
   imports = [
     ./terminals
     ./zathura.nix
   ];
+
+  config = lib.mkIf cfg.gui.enable {
+    home.packages = [
+      inputs.chiri.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
+  };
 }
