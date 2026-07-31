48668f33a82d5c2318a068b7109db6a3290629dd
willow <42willow@pm.me>
2026-07-31 18:22:43 +1000

    earthy: block sleep

--- hosts/earthy/default.nix
+++ hosts/earthy/default.nix
@@ -51,4 +51,14 @@
       enable = true;
     };
   };
+
+  services.logind.settings.Login = {
+    HandleLidSwitch = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+    HandleLidSwitchDocked = "ignore";
+  };
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
