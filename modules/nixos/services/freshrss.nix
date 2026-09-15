d48731e12d791b538dff7f8b5460be5723572488
willow <42willow@pm.me>
2026-09-15 16:01:02 +1000

    caddy and tailscale serve config

--- modules/nixos/services/freshrss.nix
+++ modules/nixos/services/freshrss.nix
@@ -1,15 +1,25 @@
-{config, ...}: {
+{config, ...}: let
+  domain = "earthy.raccoon-gourami.ts.net";
+  baseUrl = "https://${domain}";
+in {
   services.freshrss = {
     enable = true;
+    inherit baseUrl;
     api.enable = true;
     defaultUser = "willow";
     passwordFile = config.age.secrets.freshrss.file;
     webserver = "caddy";
-    baseUrl = "raccoon-gourami.ts.net";
     database = {
       type = "sqlite";
     };
   };
 
-  services.caddy.enable = true;
+  services.caddy = {
+    enable = true;
+    virtualHosts.${domain}.extraConfig = ''
+      tls {
+        get_certificate tailscale
+      }
+    '';
+  };
 }
--- modules/nixos/services/navidrome.nix
+++ modules/nixos/services/navidrome.nix
@@ -6,6 +6,10 @@
       MusicFolder = "/home/willow/media/music"; # TODO declare in options
       Address = "0.0.0.0";
       Port = 4553;
+      ExtAuth = {
+        Header = "Tailscale-User-Login";
+        TrustedSources = "127.0.0.1/32,::1/128";
+      };
     };
     plugins = with pkgs.navidromePlugins; [
       discord-rich-presence
